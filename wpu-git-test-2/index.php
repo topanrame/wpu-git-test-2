84eb1f4d9aaefde2f29bc30a5e03476e95e2da74
nathan <ramashiframa@gmail.com>
2023-09-12 13:00:58 +0700

    menambahkan baris

--- wpu-git-test-2/index.php
+++ wpu-git-test-2/index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore atque aperiam praesentium modi officiis natus nesciunt, eveniet accusamus asperiores laborum non, alias, repudiandae iusto sit ea optio quo! Odio, accusantium?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam repellat ipsum facere nostrum vitae error! Nulla non quo earum itaque officia optio eaque voluptatem in pariatur, quam aliquid libero? At.</p>
 </body>
 </html>
