3765a6a297ab195540006d4ae9565d0e1e18a5f3
nathan <ramashiframa@gmail.com>
2023-09-12 12:51:37 +0700

    tambahin p

--- wpu-git-test-2/index.php
+++ wpu-git-test-2/index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore atque aperiam praesentium modi officiis natus nesciunt, eveniet accusamus asperiores laborum non, alias, repudiandae iusto sit ea optio quo! Odio, accusantium?</p>
 </body>
 </html>
